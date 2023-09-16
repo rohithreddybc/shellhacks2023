d56e7126bd2125fd0bd08d855d2d28c5f8fcdb3f
princepatrick <princepatrickanand1996@gmail.com>
2023-09-16 16:08:16 -0400

    Changes in AppRouter
    Changes in AppRouter

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginForm from './Component/LoginForm';
 import RegisterForm from './Component/RegisterForm';
+import StudentRequestFund from './Component/StudentRequestFund';
+import StudentHome from './Component/StudentHome'
+import StudentFundHistory from './Component/StudentFundHistory';
 
 function AppRouter() {
   return (
@@ -9,6 +12,9 @@ function AppRouter() {
       <Routes>
         <Route path="/" element={<LoginForm />}/>
         <Route path="/register" element={<RegisterForm />} />
+        <Route path="/student/studentRequestFund" element={<StudentRequestFund />} />
+        <Route path="/student" element={<StudentHome />} />
+        <Route path="/student/fund-history" element={<StudentFundHistory/>} />
       </Routes>
     </Router>
   );
