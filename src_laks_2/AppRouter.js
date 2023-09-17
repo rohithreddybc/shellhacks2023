6e28b6bfce207f1e96d963827c6f832244c41d3f
princepatrick <princepatrickanand1996@gmail.com>
2023-09-16 21:50:17 -0400

    app router changes

--- src_laks_2/AppRouter.js
+++ src_laks_2/AppRouter.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import LoginForm from './component/LoginForm';
-import RegisterForm from './component/RegisterForm';
-import RegisterDonorForm from './component/RegisterDonorForm';
+import LoginForm from './Component/LoginForm';
+import RegisterForm from './Component/RegisterForm';
+import StudentRequestFund from './Component/StudentRequestFund';
+import StudentHome from './Component/StudentHome'
+import StudentFundHistory from './Component/StudentFundHistory';
+import DonorHome from './Component/DonorHome';
 
 function AppRouter() {
   return (
@@ -10,7 +13,11 @@ function AppRouter() {
       <Routes>
         <Route path="/" element={<LoginForm />}/>
         <Route path="/register" element={<RegisterForm />} />
-        <Route path="/registerDonorForm" element={<RegisterDonorForm />} />
+        <Route path="/student/studentRequestFund" element={<StudentRequestFund />} />
+        <Route path="/student" element={<StudentHome />} />
+        <Route path="/student/fund-history" element={<StudentFundHistory/>} />
+        <Route path="/student/fund-me/:studentEmail" element={<StudentFundHistory/>} />
+        <Route path="/donor" element={<DonorHome />} />
       </Routes>
     </Router>
   );
